dea746f191c8b29343c449b642e5f5a13aa0d79a
xtncz <ascendus.dev@gmail.com>
2026-05-16 00:20:29 +1200

    chore: create migration

--- src/env.js
+++ src/env.js
@@ -8,6 +8,7 @@ export const env = createEnv({
 	 */
 	server: {
 		DATABASE_URL: z.string().url(),
+		DIRECT_URL: z.string().url(),
 		NODE_ENV: z
 			.enum(["development", "test", "production"])
 			.default("development"),
@@ -31,6 +32,7 @@ export const env = createEnv({
 	 */
 	runtimeEnv: {
 		DATABASE_URL: process.env.DATABASE_URL,
+		DIRECT_URL: process.env.DIRECT_URL,
 		NODE_ENV: process.env.NODE_ENV,
 		AUTH_SECRET: process.env.AUTH_SECRET,
 		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
